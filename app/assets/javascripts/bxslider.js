46d22ded8bcee67b7566d21e9c315a7a90613e2c
Yuri <yu5kxk.g@gmail.com>
2019-02-18 12:50:46 +0000

    [update]posts/show bxslider

--- app/assets/javascripts/bxslider.js
+++ app/assets/javascripts/bxslider.js
@@ -1,10 +1,7 @@
 $(document).ready(function(){
   $('.bxslider').bxSlider({
-    controls: true,       // 前後のコントロールを表示
-    arrows : true,        //ナビゲーション矢印
-    speed: 1000,          // スライドするスピード
-    moveSlides: 4,        // 移動するスライド数
-    maxSlides: 1,         // 一度に表示させる最大数
-    slideWidth: 300,      // 各スライドの幅
+    mode: 'fade',
+    captions: true,
+    slideWidth: 300, // 各スライドの幅
   });
 });
